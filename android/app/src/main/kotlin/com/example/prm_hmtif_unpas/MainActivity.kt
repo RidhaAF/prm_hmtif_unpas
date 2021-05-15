ae0f11e1e7abee042df6f8e4df526fa31f312caa
Ridha Ahmad Firdaus <41348944+RidhaAF@users.noreply.github.com>
2021-05-15 11:59:14 +0700

    Initial commit

---
+++ android/app/src/main/kotlin/com/example/prm_hmtif_unpas/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.prm_hmtif_unpas
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+import 'package:prm_hmtif_unpas/pages/home_page.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: HomePage(),
+    );
+  }
+}
