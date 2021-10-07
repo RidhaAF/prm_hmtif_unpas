7bf685f39bc390ab3ede7bbbc835094f136baeef
Ridha Ahmad Firdaus <41348944+RidhaAF@users.noreply.github.com>
2021-10-07 11:39:31 +0700

    Add Coming Soon Page

---
+++ lib/pages/coming_soon_page.dart
@@ -0,0 +1,66 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:prm_hmtif_unpas/theme/theme.dart';
+
+class ComingSoonPage extends StatelessWidget {
+  const ComingSoonPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    Widget illustration() {
+      return Container(
+        height: 200,
+        width: double.infinity,
+        decoration: BoxDecoration(
+          image: DecorationImage(
+            image: AssetImage('assets/il_coming_soon.png'),
+          ),
+        ),
+      );
+    }
+
+    Widget descriptionText() {
+      return Text(
+        'Halaman\nbelum tersedia😢',
+        style: GoogleFonts.inter(
+          color: primaryColor,
+          fontSize: 18,
+          fontWeight: FontWeight.w600,
+        ),
+        textAlign: TextAlign.center,
+      );
+    }
+
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: primaryColor,
+        elevation: 0,
+        systemOverlayStyle: SystemUiOverlayStyle.light,
+        title: Text(
+          'Coming Soon',
+          style: GoogleFonts.inter(
+            fontSize: 18,
+            fontWeight: FontWeight.w600,
+          ),
+        ),
+        centerTitle: true,
+        automaticallyImplyLeading: true,
+      ),
+      backgroundColor: backgroundColor2,
+      body: Center(
+        child: Container(
+          margin: EdgeInsets.all(defaultMargin),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              illustration(),
+              SizedBox(height: 16),
+              descriptionText(),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
