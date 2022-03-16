1a7a18c1199f049334f9c6e52266cb5af759aabb
ridhaaf <ridhaaf@gmail.com>
2022-03-16 23:55:23 +0700

    Delete unused import

--- lib/pages/change_password_page.dart
+++ lib/pages/change_password_page.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:prm_hmtif_unpas/theme/theme.dart';
 
--- lib/pages/edit_profile_page.dart
+++ lib/pages/edit_profile_page.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:prm_hmtif_unpas/pages/home/main_page.dart';
 import 'package:prm_hmtif_unpas/providers/auth_provider.dart';
